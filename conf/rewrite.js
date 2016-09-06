6435a7652bec4f7e69e780d3a6f043e2a818d9fa
sunjia032 <sunjia032@pingan.com.cn>
2016-09-06 13:35:39 +0800

    'init'

--- conf/rewrite.js
+++ conf/rewrite.js
@@ -3,5 +3,6 @@ module.exports = {
     '^/ajax$': '/pagelet/ajax/ajax.html', //项目中的文件
     '^/ajax/callback': function(req, res){  //可以是一个函数
         res.send('hello, world!');
-    }
+    },
+    '^/home$':'/views/home.html'
 };
