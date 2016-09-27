635e6187b8e741c2b5d63fa757f8d8ee438ebf8c
sunjia032 <sunjia032@pingan.com.cn>
2016-09-27 18:51:33 +0800

    'menu'

--- conf/deploy/local.js
+++ conf/deploy/local.js
@@ -1,4 +1,11 @@
-module.exports ={
+module.exports = {
     from: '/',
-    to: '../doc-backend/doc-frontend' 
+    //本地目录
+    to: '../doc-backend/doc-frontend',
+    //include: /\/static\/.*/,    //过滤
+    subOnly: true, //只移动子文件夹
+    // replace: {  //对文件内容进行替换， 替换所有的域名
+    //     from: "//www.baidu.com",
+    //     to: "//www.tieba.com"
+    // }  
 };
